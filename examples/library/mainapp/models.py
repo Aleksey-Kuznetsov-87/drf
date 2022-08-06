46a500e07fd0bd1bc83d4a3c0fa8cfb9224f56a4
Aleksey-Kuznetsov-87 <aleckuz@yandex.ru>
2022-08-07 02:22:39 +0700

    lesson_12

--- examples/library/mainapp/models.py
+++ examples/library/mainapp/models.py
@@ -1,12 +1,14 @@
+from django.contrib.postgres.functions import RandomUUID
 from django.db import models
 from uuid import uuid4
 
 
 class Author(models.Model):
-    uid = models.UUIDField(primary_key=True, default=uuid4)
+    uid = models.UUIDField(primary_key=True, default=RandomUUID)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     birthday_year = models.PositiveIntegerField()
+#  было - uid = models.UUIDField(primary_key=True, default=uuid4)
 
 
 class Biography(models.Model):
